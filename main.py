58f2a9c844ae07b83b3f6244d2800f60f8a3e2de
Ji Li <jili5@microsoft.com>
2018-10-26 16:17:50 -0700

    preload dataset done

--- main.py
+++ main.py
@@ -2,47 +2,48 @@
 """Main Entry """
 
 # External dependencies
-from trainer.modelparam import ModelParams
-from trainer.model import Text2Vec
-import numpy as np
-import pickle as pk
+from trainer.searcher import Searcher
 
 # Authorship
 __author__ = "Ji Li"
 __email__ = "jili5@microsoft.com"
 
 # create model params
-nn_params =  [300] # for additional hidden layers
-modelParams = ModelParams(in_dim=300, max_epochs=300, batch_size=32, learning_rate=0.003, dropout=0.1, class_threshold=.5, nn_params = nn_params)
-
-# precessing
-# load data, map
-# entry format: idx, embedding, label, icon name, phrase idx
-fileObject = open("data/iconName2IndexMap.p", 'rb')
-mp_icon2idx = pk.load(fileObject)
-# print(mp_icon2idx)
-
-# embedding = "word2vec"
-# embedding = "fasttext"
-embedding = "glove"
-
-fileObject = open("data/training/train."+embedding+".p", 'rb')
-trainset = pk.load(fileObject)
-fileObject = open("data/training/dev."+embedding+".p", 'rb')
-devset = pk.load(fileObject)
-# print(len(devset), devset[0])
-fileObject = open("data/training/test."+embedding+".p", 'rb')
-testset = pk.load(fileObject)
-fileObject.close()
-# print(len(trainset), len(devset), trainset[0])
-
-
-# create model
-model = Text2Vec(modelParams, num_icons = len(mp_icon2idx), trainset = trainset, devset = devset, testset = testset)
-model.train()
+# nn_params =  [300] # for additional hidden layers
+# modelParams = ModelParams(in_dim=300, max_epochs=300, batch_size=32, learning_rate=0.003, dropout=0.1, class_threshold=.5, nn_params = nn_params)
 
 
 
+# # embedding = "word2vec"
+# # embedding = "fasttext"
+# embedding = "glove"
 
+# fileObject = open("data/training/train."+embedding+".p", 'rb')
+# trainset = pk.load(fileObject)
+# fileObject = open("data/training/dev."+embedding+".p", 'rb')
+# devset = pk.load(fileObject)
+# # print(len(devset), devset[0])
+# fileObject = open("data/training/test."+embedding+".p", 'rb')
+# testset = pk.load(fileObject)
+# fileObject.close()
+# # print(len(trainset), len(devset), trainset[0])
+
+
+
+# # create model
+# model = Text2Vec(modelParams, num_icons = len(mp_icon2idx), trainset = trainset, devset = devset, testset = testset)
+# model.train()
+
+
+# top level params to control the script
+config = {
+    "nn_params": [[200],[300],[400],[300,300]],
+    "max_epochs": [100], 
+    "learning_rate": [0.1, 0.01, 0.001, 0.0001, 0.00001], 
+    "batch_size": [4,16,32], 
+    "embedding": ["word2vec", "fasttext", "glove"]
+}
+
+searcher = Searcher(config)
 
 
--- trainer/searcher.py
+++ trainer/searcher.py
@@ -6,8 +6,8 @@ param searcher to find the best P@1 P@2
 # External dependencies
 # import tensorflow as tf
 import numpy as np
-from modelparam import ModelParams
-from model import Text2Vec
+from trainer.modelparam import ModelParams
+from trainer.model import Text2Vec
 
 # from os import environ
 # from random import shuffle
@@ -15,20 +15,20 @@ from model import Text2Vec
 # from warnings import filterwarnings 
 # filterwarnings(action='ignore', category=UserWarning, module='gensim')
 # import gensim.models as gs
-# import pickle as pk
+import pickle as pk
 
 # Authorship
 __author__ = "Ji Li"
 __email__ = "jili5@microsoft.com"
 
-# top level params to control the script
-params = {
-    "nn_params": [[200],[300],[400],[300,300]],
-    "max_epochs": [100], 
-    "learning_rate": [0.1, 0.01, 0.001, 0.0001, 0.00001], 
-    "batch_size": [4,16,32], 
-    "embedding": ["word2vec", "fasttext", "glove"]
-}
+# # top level params to control the script
+# config = {
+#     "nn_params": [[200],[300],[400],[300,300]],
+#     "max_epochs": [100], 
+#     "learning_rate": [0.1, 0.01, 0.001, 0.0001, 0.00001], 
+#     "batch_size": [4,16,32], 
+#     "embedding": ["word2vec", "fasttext", "glove"]
+# }
 
 # nn_params =  [300] # for additional hidden layers
 # modelParams = ModelParams(in_dim=300, max_epochs=300, batch_size=32, learning_rate=0.003, dropout=0.1, class_threshold=.5, nn_params = nn_params)
@@ -37,20 +37,28 @@ class Searcher:
     """Class for run train/eval using a range of parameter settings"""
 
     # define the model
-    def __init__(self):
-        self.preload_embeddings()
+    def __init__(self, config):
+        self.config = config
+        self.preload_dataset(self.config["embedding"])
+        self.preload_misc()
         
-        
-    def preload_embeddings(self):
-        # embedding = "word2vec"
-        # embedding = "fasttext"
-        embedding = "glove"
-
-        fileObject = open("data/training/train."+embedding+".p", 'rb')
-        trainset = pk.load(fileObject)
-        fileObject = open("data/training/dev."+embedding+".p", 'rb')
-        devset = pk.load(fileObject)
-        # print(len(devset), devset[0])
-        fileObject = open("data/training/test."+embedding+".p", 'rb')
-        testset = pk.load(fileObject)
-        fileObject.close()
+    def preload_dataset(self, embeddingList):
+        self.trainset = {}
+        self.devset = {}
+        self.testset = {}
+        for embedding in embeddingList:
+            fileObject = open("data/training/train."+embedding+".p", 'rb')
+            self.trainset[embedding] = pk.load(fileObject)
+            fileObject = open("data/training/dev."+embedding+".p", 'rb')
+            self.devset[embedding] = pk.load(fileObject)
+            # print(len(devset), devset[0])
+            fileObject = open("data/training/test."+embedding+".p", 'rb')
+            self.testset[embedding] = pk.load(fileObject)
+            fileObject.close()
+            
+    def preload_misc(self):
+        # load data, map
+        # entry format: idx, embedding, label, icon name, phrase idx
+        fileObject = open("data/iconName2IndexMap.p", 'rb')
+        self.mp_icon2idx = pk.load(fileObject)
+        # print(mp_icon2idx)
