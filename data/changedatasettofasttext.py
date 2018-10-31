0d0f8f5c465b867de0642db8fd5d9b3ad83c7649
Ji Li <jili5@microsoft.com>
2018-10-31 10:43:33 -0700

    working parser for fasttext format

--- data/changedatasettofasttext.py
+++ data/changedatasettofasttext.py
@@ -7,7 +7,7 @@ __email__ = "jili5@microsoft.com"
 
 # main process func
 def parse_raw_input(setname):
-    fw = open("training/fast"+setname+".fasttext.txt", "w")
+    mp_keyword2icon = {}
     with open("training/"+setname+".txt", "r") as f:
         lines = f.readlines()
         for line in lines:
@@ -20,19 +20,24 @@ def parse_raw_input(setname):
             else:
                 label = "False"
                 continue
-            print(icon, keywords, label)
-#             fw.write(" ".join(keywords))
-#             fw.write("\t")
-#             fw.write(icon)
-#             fw.write("\t")
-#             fw.write(label)
-#             fw.write("\n")
+#             print(icon, keywords, label)
+            for keyword in keywords:
+                if keyword not in mp_keyword2icon:
+                    mp_keyword2icon[keyword] = set()
+                mp_keyword2icon[keyword].add(icon[:-4])
+#     print(mp_keyword2icon)
+    fw = open("fasttext/"+setname+".fasttext.txt", "w")
+    for keyword in mp_keyword2icon:
+        line = []
+        for iconlabel in mp_keyword2icon[keyword]:
+            line += ["__label__"+iconlabel]
+        line += [keyword+ "\n"]
+        fw.write(' '.join(line))
     fw.close()
 
-# parse_raw_input("train")
-# parse_raw_input("dev")
+parse_raw_input("train")
+parse_raw_input("dev")
 parse_raw_input("test")
 
         
-# process here
 
