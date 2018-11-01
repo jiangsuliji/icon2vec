06a56155dcbe0d1ba9309fe4f9ef21f49ac8f5e4
Ji Li <jili5@microsoft.com>
2018-11-01 13:13:40 -0700

    completed the multiclass phrase embedding and label generation

--- data/changedatasettomulticlass.py
+++ data/changedatasettomulticlass.py
@@ -13,14 +13,16 @@ class preprocessMultiClassData:
     """class that generates phrase embedding and labels for multi class classification"""
     def __init__(self, setname, embedding_method):    
         self.setname = setname
+        self.embedding_method = embedding_method
         if "word2vec"in embedding_method or "word2vec+glove" in embedding_method:
-            self.w2v = Word2Vec()
-        if "fasttext" in embedding_method:
-            self.fast = FastText('fasttext/wiki-news-300d-1M.vec.bin', loadbinary=True)
-        if "glove" in embedding_method or "word2vec+glove" in embedding_method:
-            self.glove = GloVe('glove/glove.42B.300d.txt.bin', loadbinary=True)
+            self.model = Word2Vec()
+        elif "fasttext" in embedding_method:
+            self.model = FastText('fasttext/wiki-news-300d-1M.vec.bin', loadbinary=True)
+        elif "glove" in embedding_method:
+            self.model = GloVe('glove/glove.42B.300d.txt.bin', loadbinary=True)
         self.__init__icon2idx()
         
+    
     def __init__icon2idx(self):
         # load icon idx map first
         fileObject = open("iconName2IndexMap.p", 'rb')
@@ -29,45 +31,80 @@ class preprocessMultiClassData:
         self.iconNum = len(self.mp_icon2idx)
 #         print(self.iconNum)
         fileObject.close()
+    
+    
+    def genLabel(self, icons):
+        res = [0]*self.iconNum
+        for icon in icons:
+            res[self.mp_icon2idx[icon]] = 1
+        return res
 
-# main process func
-def parse_raw_input(setname, embedding_method):
-    phrase_embedding = []
+    def outPut(self):
+        fileObject = open("multiclass/"+self.setname+"."+self.embedding_method[0]+".p", "wb")
+        pk.dump(self.mydataset, fileObject)
+        fileObject.close()
+
+    
+    def proprocess(self):
+        mp_keywords2icon = {}
+        # pass 1: parse the input dataset
+        with open("training/"+self.setname+".txt", "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                items = line.rstrip().split()
+                icon = items[0]
+                keywords = items[1:-1]
+                label = items[-1]
+                if label == "1":
+                    label = "True"
+                else:
+                    label = "False"
+                    continue
+                keywords = ' '.join(keywords)
+#                 print(icon, keywords, label)
+                if keywords not in mp_keywords2icon:
+                    mp_keywords2icon[keywords] = set()
+                    mp_keywords2icon[keywords].add(icon)
+                elif self.setname == "train":
+                    mp_keywords2icon[keywords].add(icon) 
+                else:
+                    # for test and dev, there should be no repeat
+                    raise
+#         print(mp_keywords2icon)
+        self.mydataset = []
+        # pass 2: generate icon and phrase embedding
+        for keywords, icons in mp_keywords2icon.items():
+#             print(keywords, icons)
+            label = self.genLabel(icons)
+            phrase_embedding = self.model[keywords.split()]
+            # entries for multiclass dataset
+            self.mydataset.append([np.array(phrase_embedding), np.array(label), keywords, icons])
+#         print(self.mydataset)
+        self.outPut()
+        print("processed ",self.embedding_method, self.setname, "with", len(self.mydataset), "entries")
 
         
-    mp_keywords2icon = {}
-    with open("training/"+setname+".txt", "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            items = line.rstrip().split()
-            icon = items[0]
-            keywords = items[1:-1]
-            label = items[-1]
-            if label == "1":
-                label = "True"
-            else:
-                label = "False"
-                continue
-            keywords = ' '.join(keywords)
-            #             print(icon, keywords, label)
-            if keywords not in mp_keywords2icon:
-                mp_keywords2icon[keywords] = set()
-                mp_keywords2icon[keywords].add(icon)
-            elif setname == "train":
-                mp_keywords2icon[keywords].add(icon) 
-            else:
-                # for test and dev, there should be no repeat
-                raise
-#     print(mp_keywords2icon)
-#     fw = open("multiclass/"+setname+".txt", "w")
-#     for keyword in mp_keyword2icon:
-#         line = []
-#         for iconlabel in mp_keyword2icon[keyword]:
-#             line += ["__label__"+iconlabel]
-#         line += [keyword+ "\n"]
-#         fw.write(' '.join(line))
-#     fw.close()
 
 # process here
+M = preprocessMultiClassData("train", ["word2vec"])
+M.proprocess()
+M = preprocessMultiClassData("dev", ["word2vec"])
+M.proprocess()
 M = preprocessMultiClassData("test", ["word2vec"])
+M.proprocess()
 
+
+M = preprocessMultiClassData("train", ["fasttext"])
+M.proprocess()
+M = preprocessMultiClassData("dev", ["fasttext"])
+M.proprocess()
+M = preprocessMultiClassData("test", ["fasttext"])
+M.proprocess()
+
+
+M = preprocessMultiClassData("train", ["glove"])
+M.proprocess()
+M = preprocessMultiClassData("dev", ["glove"])
+M.proprocess()
+M = preprocessMultiClassData("test", ["glove"])
+M.proprocess()
