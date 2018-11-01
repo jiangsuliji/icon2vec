55a235b8205407c194911c997230dff002b5a3fc
Ji Li <jili5@microsoft.com>
2018-11-01 14:48:44 -0700

    prepare for multi class

--- main.py
+++ main.py
@@ -3,24 +3,31 @@
 
 # External dependencies
 from trainer.searcher import Searcher
+from trainer.model import Text2VecMulti
 
 # Authorship
 __author__ = "Ji Li"
 __email__ = "jili5@microsoft.com"
 
-config = {
-    "nn_params": [[600]],
-    "max_epochs": [1000], 
-    "learning_rate": [0.003], 
-    "batch_size": [1024], 
-    "dropout": [0.0],
-    "verbose": [True],
-#     "embedding": ["word2vec"]
-#     "embedding": [["word2vec","glove"]]
-#     "embedding": ["glove"]
-    "embedding": ["word2vec-glove"]
-}
+# # searcher logic for the non-multi-class embedding
+# config = {
+#     "nn_params": [[600]],
+#     "max_epochs": [1000], 
+#     "learning_rate": [0.003], 
+#     "batch_size": [1024], 
+#     "dropout": [0.0],
+#     "verbose": [True],
+# #     "embedding": ["word2vec"]
+# #     "embedding": [["word2vec","glove"]]
+# #     "embedding": ["glove"]
+#     "embedding": ["word2vec-glove"]
+# }
 
-searcher = Searcher(config)
-searcher.run()
+# searcher = Searcher(config)
+# searcher.run()
 
+# multi-class embedding
+modelParams = ModelParams(in_dim=300, max_epochs=1000, batch_size=32, learning_rate=0.003, class_threshold=0.5, dropout=0.1, nn_params = [])
+
+M = Text2VecMulti(modelParams = modelParams, trainset = trainset, devset = devset, testset = testset)
+M.train()
--- trainer/model.py
+++ trainer/model.py
@@ -250,95 +250,34 @@ class Text2Vec:
         ops.reset_default_graph()
         tf.reset_default_graph()
 
-
+#-----------------------------------------------------------------------
+#                  multi class classifier
+#-----------------------------------------------------------------------
 class Text2VecMulti:
     """class for multi class model"""
-    def __init__(self, model_params, num_icons, trainset, devset, testset):
-        """Constructor for the Text2Vec model
-
-        Args:
-            model_params: Parameters for the model
-            num_icons: Number of icons we will ultimately train
-        """
+    def __init__(self, model_params, trainset, devset, testset):
         self.initializeDataset(trainset, devset, testset)
-        
         self.model_params = model_params
-        self.num_cols = num_icons
-        
-        # row - phrase input to the graph
-        self.phrase_vec = tf.placeholder(tf.float32, shape=[None, model_params.in_dim], name='phrase_vec')
-            
-        # Correlation between an icon and a phrase
-        self.y = tf.placeholder(tf.float32, shape=[None], name='y')
 
-        # Icon indices in current batch
-        self.col = tf.placeholder(tf.int32, shape=[None], name='col')
         
-        # 1st pass: init V embeddings for icons - multi layers
-        # Column embeddings (here icon representations)
-        self.V = []
-        for idx, num in enumerate(model_params.nn_params):
-            V = tf.get_variable("V"+str(idx), shape=[num_icons, num], initializer=tf.contrib.layers.xavier_initializer())  
-#             V = tf.Variable(tf.random_uniform([num_icons, num], -0.1, 0.1), name="V"+str(idx))  
-            self.V.append(V)
-#         print(self.V)
-                
-        # 2nd pass: calculate score, build projection matrix P between V layers
-        prev = model_params.in_dim
-        score = self.phrase_vec
-        layers = len(model_params.nn_params)
-        for idx in range(layers):
-            if model_params.in_dim != model_params.nn_params[0] or idx > 0:
-#                 P = tf.Variable(tf.random_uniform([prev, model_params.nn_params[idx]]), name="P"+str(idx))
-                P = tf.get_variable("P"+str(idx), shape=[prev, model_params.nn_params[idx]], initializer=tf.contrib.layers.xavier_initializer())
-                prev = model_params.nn_params[idx]
-                score = tf.matmul(score, P)
-#                 score = tf.tanh(tf.matmul(score, P))
-#                 print("\n")
-#                 print("P",P)
-#                 print("score", score)
-            
-            V = tf.nn.embedding_lookup(self.V[idx], self.col)
-#             print("V",V)
-            if idx != layers - 1:
-                score = tf.multiply(score, V)
-            else:
-                score = tf.multiply(score, tf.nn.dropout(V, (1-model_params.dropout)))
-#             print("score", score)
-#             print("\n")
-#         print(score)
         
-    	# custom loss
-        self.score = tf.reduce_sum(score, 1)
-
-        # Probability of match
-        self.prob = tf.sigmoid(self.score)
-        # Calculate the cross-entropy loss
-#         self.loss = tf.nn.weighted_cross_entropy_with_logits(targets=self.score, logits=self.y, pos_weight=1)
-        self.loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=self.score, labels=self.y) 
         
     def initializeDataset(self, trainset, devset, testset):
-        icon_idx, phrase_embedding, labels = [], [], []
-        ph_idx = []
-        for item in trainset:
-            icon_idx.append(item[0])
-            phrase_embedding.append(item[1])
-            labels.append(item[2])
-        self.trainset = [np.array(icon_idx), np.array(phrase_embedding), np.array(labels)]
+        pass
         
-        icon_idx, phrase_embedding, labels = [], [], []
-        for item in devset:
-            icon_idx.append(item[0])
-            phrase_embedding.append(item[1])
-            labels.append(item[2])
-        self.devset = [np.array(icon_idx), np.array(phrase_embedding), np.array(labels)]
+#         icon_idx, phrase_embedding, labels = [], [], []
+#         for item in devset:
+#             icon_idx.append(item[0])
+#             phrase_embedding.append(item[1])
+#             labels.append(item[2])
+#         self.devset = [np.array(icon_idx), np.array(phrase_embedding), np.array(labels)]
         
-        icon_idx, phrase_embedding, labels = [], [], []
-        for item in testset:
-            icon_idx.append(item[0])
-            phrase_embedding.append(item[1])
-            labels.append(item[2])
-        self.testset = [np.array(icon_idx), np.array(phrase_embedding), np.array(labels)]
+#         icon_idx, phrase_embedding, labels = [], [], []
+#         for item in testset:
+#             icon_idx.append(item[0])
+#             phrase_embedding.append(item[1])
+#             labels.append(item[2])
+#         self.testset = [np.array(icon_idx), np.array(phrase_embedding), np.array(labels)]
         
         
     def initializeSession(self):
@@ -350,51 +289,7 @@ class Text2VecMulti:
     # train the model using the appropriate parameters
     def train(self):
         """Train the model on a given knowledge base"""
-        self.optimizer = tf.train.AdamOptimizer(self.model_params.learning_rate)
-        minimization_op = self.optimizer.minimize(self.loss)
-        
-        self.initializeSession()
-        epoch = 0
-        total_data_entry = self.trainset[0].shape[0]
-        half_data_entry = self.trainset[0].shape[0]//2
-        max_accuracy_top2 = [[0,0],[0,0]] 
-        while epoch < self.model_params.max_epochs:   
-#             print(total_data_entry, half_data_entry)
-            training_idx_pos = np.random.randint(half_data_entry, size=self.model_params.batch_size//2)
-            training_idx_neg = np.random.randint(half_data_entry, size=self.model_params.batch_size//2)
-            training_idx_neg = [i+ half_data_entry for i in training_idx_neg]
-            training_idx = np.concatenate((training_idx_pos, training_idx_neg),axis =0)
-            shuffle(training_idx)
-#             y = [1 if i < half_data_entry else 0 for i in training_idx]
-            y = [self.trainset[2][i] for i in training_idx]
-            
-#             print(training_idx_neg, training_idx_pos)
-#             print("===",training_idx)
-#             print(y)
-        
-            _, current_loss = self.session.run([minimization_op, self.loss], feed_dict={
-                self.col:self.trainset[0][training_idx],
-                self.phrase_vec: self.trainset[1][training_idx],
-                self.y:np.array(y)
-            })
-            current_loss = sum(current_loss)
-
-            print("Epoch=%d loss=%3.1f" %(epoch, current_loss))
-            if epoch % 10 == 0:
-                devres = self.cal_top_n(self.devset, "dev", N=2)
-                testres = self.cal_top_n(self.testset, "test", N=2)
-            
-            # now only record the entry when it hits the maximum devset P@1
-            if devres and testres:
-                if devres[1] > max_accuracy_top2[0][1]:
-                    max_accuracy_top2 = [devres, testres]
-            epoch += 1
-            
-        print("results when max dev accu:")
-        print(max_accuracy_top2)
-        self.print_top_accuracy(max_accuracy_top2[0],"dev") 
-        self.print_top_accuracy(max_accuracy_top2[1],"test") 
-        return max_accuracy_top2
+        pass 
     
     # find top N icon indices and return P,R,F1,TP,TN,FP,FN
     def cal_top_n(self, dataset, str, N=2):
@@ -437,15 +332,8 @@ class Text2VecMulti:
         self.print_top_accuracy_TP(P, T, F, str)
         return P
     
-    # train set evaluation
-    # TODO: update
     def test_on_train(self):
-        res = self.session.run(self.prob, feed_dict={
-                self.col:self.trainset[0],
-                self.phrase_vec: self.trainset[1]
-        })
-        y_pred = [1 if y > self.model_params.class_threshold else 0 for y in res]
-        return y_pred, self.trainset[2]
+        pass
        
     
     def print_top_accuracy_TP(self, P, T, F, st):
