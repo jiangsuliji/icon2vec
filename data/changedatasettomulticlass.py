5aa25df62f6723056e8b6bdcd2e714be990b9355
Ji Li <jili5@microsoft.com>
2018-11-01 11:28:10 -0700

    make multiclass preprocessor oop

--- data/changedatasettomulticlass.py
+++ data/changedatasettomulticlass.py
@@ -9,8 +9,32 @@ from pretrained_embeddings import GloVe
 __author__ = "Ji Li"
 __email__ = "jili5@microsoft.com"
 
+class preprocessMultiClassData:
+    """class that generates phrase embedding and labels for multi class classification"""
+    def __init__(self, setname, embedding_method):    
+        self.setname = setname
+        if "word2vec"in embedding_method or "word2vec+glove" in embedding_method:
+            self.w2v = Word2Vec()
+        if "fasttext" in embedding_method:
+            self.fast = FastText('fasttext/wiki-news-300d-1M.vec.bin', loadbinary=True)
+        if "glove" in embedding_method or "word2vec+glove" in embedding_method:
+            self.glove = GloVe('glove/glove.42B.300d.txt.bin', loadbinary=True)
+        self.__init__icon2idx()
+        
+    def __init__icon2idx(self):
+        # load icon idx map first
+        fileObject = open("iconName2IndexMap.p", 'rb')
+        self.mp_icon2idx = pk.load(fileObject)
+#         print(self.mp_icon2idx)
+        self.iconNum = len(self.mp_icon2idx)
+#         print(self.iconNum)
+        fileObject.close()
+
 # main process func
-def parse_raw_input(setname):
+def parse_raw_input(setname, embedding_method):
+    phrase_embedding = []
+
+        
     mp_keywords2icon = {}
     with open("training/"+setname+".txt", "r") as f:
         lines = f.readlines()
@@ -34,7 +58,7 @@ def parse_raw_input(setname):
             else:
                 # for test and dev, there should be no repeat
                 raise
-    print(mp_keywords2icon)
+#     print(mp_keywords2icon)
 #     fw = open("multiclass/"+setname+".txt", "w")
 #     for keyword in mp_keyword2icon:
 #         line = []
@@ -45,16 +69,5 @@ def parse_raw_input(setname):
 #     fw.close()
 
 # process here
-
-# load icon idx map first
-fileObject = open("iconName2IndexMap.p", 'rb')
-mp_icon2idx = pk.load(fileObject)
-# print(mp_icon2idx)
-fileObject.close()
-
-# parse_raw_input("train")
-# parse_raw_input("dev")
-parse_raw_input("test")
-
-
+M = preprocessMultiClassData("test", ["word2vec"])
 
